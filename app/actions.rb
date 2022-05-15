482f9231c54066349c87c459e680f00ed20b6762
gab-rodriguez <gab.rodriguez@shopify.com>
2022-05-15 20:36:40 +0000

    W5D1

--- app/actions.rb
+++ app/actions.rb
@@ -1,6 +1,36 @@
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+end
+
 get '/' do
   @finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
+end
+
+get '/login' do
+  erb(:login)
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+
+  user = User.find_by(username: username)  
+
+  if user && user.password == password
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    @error_message = "Login failed."
+    erb(:login)
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect to('/')
 end
 
 #render signup.erb when user navigates to /signup path
@@ -11,25 +41,16 @@ end
 
 #show below text when the form is submitted
 post '/signup' do
-
-  # grab user input values from params
   email      = params[:email]
   avatar_url = params[:avatar_url]
   username   = params[:username]
   password   = params[:password]
 
-  # instantiate a User
-  user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
-  # if user validations pass and user is saved
-  if user.save
-
-    # return readable representation of User object
-    escape_html user.inspect
-
+  if @user.save
+    redirect to('/login')
   else
-
-    # display error messages
-    escape_html user.errors.full_messages
+    erb(:signup)
   end
 end
